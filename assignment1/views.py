cf1b6e844388e3b73582d56e18557d7e6e92b569
oghenekevbe <oghenekevbe.egume@gmail.com>
2023-09-08 21:42:02 +0100

    commiy

--- assignment1/views.py
+++ assignment1/views.py
@@ -7,12 +7,14 @@ def api(request):
     slack_name =  request.GET.get('slack_name', '')
     track =  request.GET.get('track', '')
 
-    utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    utc_time = datetime.utcnow()
+    formatted_utc_time = utc_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+
     day_of_the_week = utc_time.strftime('%A')
     response = {
         "slack_name" : slack_name,
         "current_day": day_of_the_week,
-        "utc_time": utc_time.isoformat()[:-3] + 'Z',
+        "utc_time": formatted_utc_time,
         "track" : track,
         "github_file_url" : "https://github.com/Oghenekevbe/HNG1/blob/main/assignment1/views.py",
         "github_repo_url" : "https://github.com/Oghenekevbe/HNG1",
