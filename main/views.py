a24741992f9ccb0e642917a1f56a989efe511947
oghenekevbe <oghenekevbe.egume@gmail.com>
2024-07-02 16:41:47 +0100

    done

--- main/views.py
+++ main/views.py
@@ -2,37 +2,44 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import JsonResponse
 import public_ip as ip
-import geocoder
-import python_weather
-from asgiref.sync import async_to_sync
+import ipinfo
+import requests
 
+OPENWEATHERMAP_API_KEY = '3399b556c2d716eb276740e0094f39c7'
+access_token = 'a66a244ac36a36'
+handler = ipinfo.getHandler(access_token)
 
 class HelloView(APIView):
-    @async_to_sync
-    async def get(self, request):
+    
+    def get(self, request):
         # Extract visitor_name from request
         visitor_name = request.GET.get('visitor_name', "").strip('()').strip('"') or 'World'
 
         # Get the client's public IP address
         client_ip = ip.get()
         
+        # #for python anywhere only 
+        # client_ip = request.META.get('HTTP_X_REAL_IP')
+
+        
         # Get the client's location based on the IP address
-        client_location = geocoder.ip(client_ip)
+        client_location = handler.getDetails(client_ip)
+        city = client_location.city
+        print('city = ', city)
 
-        # Define the async function to get the weather
-        async def get_weather(city):
-            async with python_weather.Client() as client:
-                weather = await client.get(city)
-                return weather
+        # Get weather data from OpenWeatherMap
+        weather_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHERMAP_API_KEY}&units=metric"
+        weather_response = requests.get(weather_url)
+        weather_data = weather_response.json()
 
-        # Run the async function to get the weather
-        weather_data = await get_weather(client_location.city)
+        # Extract temperature and weather description
+        temperature = weather_data['main']['temp']
 
         # Prepare the response data
         response_data = {
-            'client_ip': client_ip,
-            'location': client_location.city,
-            "greeting": f"Hello, {visitor_name}!, the temperature is {weather_data.temperature} degrees Celcius in {client_location.city}"
-        }
+                    'client_ip': client_ip,
+                    'location': client_location.city,
+                    "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {client_location.city}"
+                }
 
         return JsonResponse(response_data)
